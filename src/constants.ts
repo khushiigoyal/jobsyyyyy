bb9a3ae05de347e85797a6b3813cf3c764da563e
khushiigoyal <khushigoyal0508452@gmail.com>
2025-12-27 20:45:15 +0530

    Create constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+import { Question } from './types';
+
+export const SAMPLE_RESUME = `Jane Doe ...`; // Paste your full Sample Resume string here
+export const SAMPLE_JOB_DESC = `Job Title: Principal Product Manager...`; // Paste your full Job Desc here
+export const APTITUDE_QUESTIONS: Question[] = [ ... ]; // Paste your full questions array here
