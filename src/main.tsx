1c145febdfbc4fc8934bd3d10bcacf1c093ef232
khushiigoyal <khushigoyal0508452@gmail.com>
2025-12-27 22:26:24 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Make sure this file exists or remove this line
+import './index.css';
 
+// We add a fail-safe check here
 const rootElement = document.getElementById('root');
 
 if (!rootElement) {
-  throw new Error("Failed to find the root element. Make sure index.html has <div id='root'></div>");
+  console.error("Critical Error: Root element not found in index.html");
+} else {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<App />);
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
